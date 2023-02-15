0457fcc708e550c629b2b4be3c56ab38fe5ff3db
Milner Chen <milnerchen0@gmail.com>
2023-02-15 15:25:03 -0500

    finish phase 1

--- Pioneering/config/routes.rb
+++ Pioneering/config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :users
+  get 'users', to: 'users#index'
+  get 'users/new', to: 'users#new', as: 'new_user'
+  post 'users', to: 'users#create'
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/:id/edit', to: 'users#edit'
+  patch 'users/:id', to: 'users#update'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
+
 end
