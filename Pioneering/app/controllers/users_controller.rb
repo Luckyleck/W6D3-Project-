bdb8a4797942870b139ebe253620c455399fd490
Alex L <luckyleck29@gmail.com>
2023-02-15 16:40:27 -0500

    Phase 3

--- Pioneering/app/controllers/users_controller.rb
+++ Pioneering/app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
 
+    def index
+        
+
+        render json: "Hello, you're on rails!"
+    end
+
+    def create
+
+        render json: params
+        
+    end
+
+    def show
+
+        render json: params
+
+    end
+
 
 end
