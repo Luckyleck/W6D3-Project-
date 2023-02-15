a9ec623c37ec90bf48c3bca4322cda69ab444d3a
Milner Chen <milnerchen0@gmail.com>
2023-02-15 17:27:31 -0500

    show strong params

--- Pioneering/app/controllers/users_controller.rb
+++ Pioneering/app/controllers/users_controller.rb
@@ -2,20 +2,35 @@ class UsersController < ApplicationController
 
     def index
         
-
-        render json: "Hello, you're on rails!"
+        @users = User.all
+        render json: @users
+        # render json: "Hello, you're on rails!"
     end
 
     def create
 
-        render json: params
+        # render json: params
+        @user = User.new(strong_params)
+        if @user.save
+            render json: @user
+        else
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
         
     end
 
     def show
+        @user = User.find(params[:id])
+        if @user
+            render json: @user
+        else
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
 
-        render json: params
+    end
 
+    def strong_params
+        params.require(:user).permit(:name, :email)
     end
 
 
